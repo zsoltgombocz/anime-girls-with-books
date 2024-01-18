8ae371998f2edfc78fc65259ced826c9b78e55bc
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-18 18:48:59 +0100

    basic config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+    darkMode: 'class',
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                logo: 'Akshar, sans-serif',
+            },
+            colors: {
+                primary: colors.blue['600'],
+                gray: {
+                    primary: colors.gray['800'],
+                    secondary: colors.gray['700'],
+                },
+                white: {
+                    primary: colors.neutral['200'],
+                    secondary: colors.neutral['300'],
+                },
+            },
+        },
     },
     plugins: [],
 };
