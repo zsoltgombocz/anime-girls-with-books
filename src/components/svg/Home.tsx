33dcc3d9a0ac87a318f12fb7da44d9eaf0e3891e
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-18 18:48:07 +0100

    Custom icons for menu

---
+++ src/components/svg/Home.tsx
@@ -0,0 +1,52 @@
+import { motion as m } from 'framer-motion';
+
+export default function Home() {
+    return (
+        <m.svg
+            xmlns={'http://www.w3.org/2000/svg'}
+            width={'35'}
+            height={'35'}
+            viewBox={'0 0 11 11'}
+            fill={'none'}
+            stroke={'inherit'}
+            className={'icon'}
+        >
+            <g clipPath={'url(#clip0_201_11088)'}>
+                <path
+                    d={
+                        'M1.81787 4.96143V9.45972C1.81787 9.54061 1.85 9.61818 1.90719 9.67537C1.96439 9.73256 2.04196 9.76469 2.12284 9.76469H3.95266V7.17245C3.95266 7.05113 4.00085 6.93477 4.08664 6.84899C4.17243 6.7632 4.28879 6.715 4.41011 6.715H5.93496C6.05628 6.715 6.17264 6.7632 6.25843 6.84899C6.34422 6.93477 6.39241 7.05113 6.39241 7.17245V9.76469H8.22223C8.30311 9.76469 8.38068 9.73256 8.43787 9.67537C8.49507 9.61818 8.5272 9.54061 8.5272 9.45972V4.96143'
+                    }
+                    strokeWidth={'0.609939'}
+                    strokeLinecap={'round'}
+                    strokeLinejoin={'round'}
+                    stroke={'inherit'}
+                />
+                <path
+                    d={
+                        'M9.44197 5.80008L5.37997 1.91172C5.28467 1.81108 5.06185 1.80994 4.96483 1.91172L0.902832 5.80008'
+                    }
+                    strokeWidth={'0.609939'}
+                    strokeLinecap={'round'}
+                    strokeLinejoin={'round'}
+                    stroke={'inherit'}
+                />
+                <path
+                    d={'M7.91735 4.33235V2.14038H7.00244V3.45556'}
+                    strokeWidth={'0.609939'}
+                    strokeLinecap={'round'}
+                    strokeLinejoin={'round'}
+                    stroke={'inherit'}
+                />
+            </g>
+            <defs>
+                <clipPath id={'clip0_201_11088'}>
+                    <rect
+                        width={'9.75902'}
+                        height={'9.75902'}
+                        transform={'translate(0.292969 0.920532)'}
+                    />
+                </clipPath>
+            </defs>
+        </m.svg>
+    );
+}
