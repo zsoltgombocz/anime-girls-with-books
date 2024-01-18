94230b497b1c56fee97946ad03c3f260eed78eb6
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-18 20:11:27 +0100

    WIP: information page

--- src/views/Informations.tsx
+++ src/views/Informations.tsx
@@ -1,3 +1,12 @@
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import PageTitle from '../components/PageTitle';
+import Information from '../components/svg/Information';
+
 export default function Informations() {
-    return <div>{'Informations'}</div>;
+    return (
+        <>
+            <PageTitle title={'information'} icon={<InformationCircleIcon />} />
+            <section>{'info text'}</section>
+        </>
+    );
 }
