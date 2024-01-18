36d7dcc94594c0252b629974bb3fc039f475fd2e
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-18 18:48:21 +0100

    Routes added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Component imports
+import Layout from './Layout';
+import Index from './views/Index';
+import Informations from './views/Informations';
+import Page404 from './views/404';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                id: 'home',
+                path: '/',
+                element: <Index />,
+            },
+            {
+                id: 'informations',
+                path: '/information',
+                element: <Informations />,
+            },
+            {
+                path: '*',
+                element: <Page404 />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <div className={'bg-blue-200 w-screen h-screen'}>{'text'}</div>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
---
+++ src/views/404.tsx
@@ -0,0 +1,16 @@
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import Button from '../components/Button';
+
+export default function Page404() {
+    return (
+        <div className={'w-full flex flex-col justify-center items-center'}>
+            <h1 className={'text-[5rem] font-bold leading-[5rem]'}>{'404'}</h1>
+            <span className={'text-3xl mb-10'}>{'Page not found!'}</span>
+            <Button
+                text={'Back to Home'}
+                redirect={'/'}
+                icon={<ArrowLeftIcon className={'h-6 w-6'} />}
+            />
+        </div>
+    );
+}
