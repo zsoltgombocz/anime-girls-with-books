aebf115b429c6015ed4dffbb11be58bed932ffea
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-18 18:48:32 +0100

    Create separated interface file

---
+++ src/interfaces.ts
@@ -0,0 +1,18 @@
+import { TargetAndTransition, VariantLabels, Variants } from 'framer-motion';
+import { ReactElement } from 'react';
+
+export interface MenuElementInterface {
+    whileHover?: VariantLabels | TargetAndTransition | undefined;
+    variants?: Variants | undefined;
+    initial?: boolean | VariantLabels | undefined;
+    Icon: React.ComponentType<any>;
+    label?: string | undefined;
+    isActive?: boolean;
+    to?: string | undefined;
+}
+
+export interface ButtonInterface {
+    text: string;
+    redirect?: string | undefined;
+    icon?: ReactElement | undefined;
+}
