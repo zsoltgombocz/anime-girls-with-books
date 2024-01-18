f6afcc8da58bdeec0221a829c6b2dbdcf0950b77
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-01-18 20:11:39 +0100

    404 page fixed centering

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -3,7 +3,11 @@ import Button from '../components/Button';
 
 export default function Page404() {
     return (
-        <div className={'w-full flex flex-col justify-center items-center'}>
+        <div
+            className={
+                'w-full h-full flex flex-col justify-center items-center'
+            }
+        >
             <h1 className={'text-[5rem] font-bold leading-[5rem]'}>{'404'}</h1>
             <span className={'text-3xl mb-10'}>{'Page not found!'}</span>
             <Button
