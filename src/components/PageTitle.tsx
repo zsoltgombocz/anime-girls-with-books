a0972031c68d71ad7ebd5ac0c6325c4e3cc01c0f
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2024-03-13 18:46:52 +0100

    Tweak icon color + less margin on mobile

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -4,7 +4,7 @@ export default function PageTitle({ title, icon }: PageTitleInterface) {
     return (
         <div
             className={
-                'w-full flex items-center justify-center pt-12 pb-6 relative mb-20'
+                'w-full flex items-center justify-center pt-12 pb-6 relative mb-14 md:mb-20'
             }
         >
             <h1 className={'relative text-4xl lg:text-6xl uppercase z-20'}>
@@ -12,7 +12,7 @@ export default function PageTitle({ title, icon }: PageTitleInterface) {
             </h1>
             <div
                 className={`z-10 absolute -bottom-22 left-1/2 -translate-x-1/2 w-[200px] lg:w-[300px] 
-                    lg:h-auto aspect-square dark:text-gray-primary/10 text-white-primary/30`}
+                    lg:h-auto aspect-square dark:text-white-primary/10 text-gray-primary/10`}
             >
                 {icon}
             </div>
